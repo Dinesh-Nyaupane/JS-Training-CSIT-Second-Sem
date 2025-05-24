a9c8312e292cd2f6b3b52da70404af9a2163c119
unknown <samip509s@gmail.com>
2025-05-24 12:14:55 +0545

    done

---
+++ index.js
@@ -0,0 +1,13 @@
+function oddeve(a)
+{
+  if(a%2==0)
+  {
+    console.log("even")
+  }
+  else{
+    console.log("odd")
+  }
+}
+let a=parseInt(prompt("enter the number"))
+let b=oddeve(a);
+console.log(a)
