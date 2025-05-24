34b7db9340d049ac326e22081e9e6fbcf839cf57
AasishPokhrel <aasishpokharel0@gmail.com>
2025-05-24 12:33:15 +0545

    Add index.html and script.js for assignment 9

---
+++ index.js
@@ -0,0 +1,8 @@
+// odd even checking
+const num = Number(prompt("enter number:"));
+if (num % 2 == 0) {
+    console.log(num + " is even");
+    } else {
+        console.log(num + " is odd");
+        }
+        
