628ce88aeb42f04947fec1eeb9424c22cb685b52
unknown <aadarsha1234thapa@gmail.com>
2025-05-24 20:44:39 +0545

    aaded odd even checking program

---
+++ script.js
@@ -0,0 +1,15 @@
+let n;
+function oddEven(n)
+{
+    if(n%2==0)
+    {
+        alert("entered number is an even number")
+    }
+    else
+    {
+        alert("entered number is a odd number")
+    }
+}
+n=parseInt(prompt("enter any number"));
+oddEven();
+
