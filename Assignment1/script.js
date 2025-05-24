ae2a9724416bd24f3b6fdc2f5e4e1731502f5ef8
Dinesh Nyaupane <dineshnyaupane333@gmail.com>
2025-05-24 12:29:28 +0545

    Update script.js

--- Assignment1/script.js
+++ Assignment1/script.js
@@ -1,23 +1,18 @@
-
-//function to check odd/even that returns true for even and false for odd.
-function oddEvenChecker(a) {
-    if (a % 2 == 0) {
-        return true;
-    } else {
-        return false
-    }
+// Function to check even numbers (returns true for even, false for odd)
+function isEven(a) {
+    return a % 2 === 0;
 }
 
-//taking number from user
+// Taking number from user
 let num = parseInt(prompt("Enter any valid positive number"));
 
-//initially checking is the input is a number or not
+// Validating user input
 if (isNaN(num)) {
-    alert("Please enter a valid number....")
+    alert("Please enter a valid number.");
 } else {
-    if (oddEvenChecker(num)==true) {  //condition when function returns true
-        alert("The number is even...")
-    } else if (oddEvenChecker(num)==false) { //condition when function is not true
-        alert("The given number is odd...")
+    if (isEven(num)) {
+        alert("The number is even.");
+    } else {
+        alert("The given number is odd.");
     }
 }
