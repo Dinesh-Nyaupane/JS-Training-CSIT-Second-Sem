c87386e7956a80e014b3bc99f2e61b7e974e930a
unknown <bhattaraibrikshya92@gmail.com>
2025-05-31 10:39:06 +0545

    first commit to the branch

---
+++ script.js
@@ -0,0 +1,10 @@
+// Function to check odd or even
+function OddEven(num) {
+  if (num % 2 === 0) {
+    alert(num + " is Even");
+  } else {
+    alert(num + " is Odd");
+  }
+}
+OddEven(5); 
+OddEven(12); 
