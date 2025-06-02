6656ecad31e2533ab8a5f7dc35324ed28181ed31
peoniesnpista <payalpulami@gmail.com>
2025-06-03 00:01:32 +0545

    First commit to the branch

---
+++ index.js
@@ -0,0 +1,11 @@
+// odd-even-check
+
+function checkOddEven(number) {
+    if (number % 2 === 0) {
+        console.log(number + " is Even");
+    } else {
+        console.log(number + " is Odd");
+    }
+}
+
+
