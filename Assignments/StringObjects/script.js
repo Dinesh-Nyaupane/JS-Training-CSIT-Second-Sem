55907e9b201d6262d73d7779a57e6e3d860969a2
unknown <samikshyawagle20@gmail.com>
2025-06-23 14:22:30 +0545

    Initial commit

---
+++ Assignments/StringObjects/script.js
@@ -0,0 +1,28 @@
+//count the total number of character in string
+let names = "samikshya wagle"
+console.log(names.length)
+
+//get the first and last character 
+let username = "Hydrogen"
+console.log(username.charAt(0)&&username.charAt(username.length-1))
+
+// to convert into uppercase
+let word = "samikshya" ;
+console.log(word.toUpperCase());
+
+//to lowercase
+let sentence = "SAMIKSHYA";
+console.log(sentence.toLowerCase());
+
+//remove extra space around the text
+let have = " higher "
+console.log(have.trim());
+ 
+//ectract character from string using slice
+let city = kathmandu;
+console.log(city.substring(2,5));
+
+//8. Repeat a Word Multiple Times
+let consider = "samikshya";
+console.log(consider.repeat(3))
+
