dc40281d4016c3fb046d4d213032250999d29f13
Sandesh Aryal <sandesharyal2062@gmail.com>
2025-06-25 14:43:28 +0545

    class task of array

--- stringmanipulating/masktheusername.js
+++ stringmanipulating/masktheusername.js
@@ -1,11 +1,10 @@
-let email_value="sandesharyal@gmail.com";
-let position_of=email_value.indexOf("@");
-let split_to_first=email_value.slice(0, position_of);
-let split_to_second=email_value.slice(position_of);
-let loot_to=split_to_first.length;
-let replace=split_to_first;
-for(let i=1;i<loot_to;i++){
- replace=replace.replaceAll(replace[i],"*");
+let email_value = "sandesharyal@gmail.com";
+let position_of = email_value.indexOf("@");
+let split_to_first = email_value.slice(0, position_of);
+let split_to_second = email_value.slice(position_of);
+let loot_to = split_to_first.length;
+let replace = split_to_first[0];
+for (let i = 1; i <= loot_to; i++) {
+  replace = replace + "*";
 }
-console.log(replace+split_to_second);
-
+console.log(replace + split_to_second);
