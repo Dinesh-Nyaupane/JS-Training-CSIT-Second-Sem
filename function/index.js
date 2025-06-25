aed7525a2488bb5982a31902ef15059949a28749
unknown <samip509s@gmail.com>
2025-06-25 14:18:20 +0545

    task

--- function/index.js
+++ function/index.js
@@ -1,4 +1,4 @@
-// ////1///////
+////1///////
 // let remainder=1;
 // let sum=0;
 // function sumd(a){
@@ -15,84 +15,76 @@
 
 
 
-// // // // // // ////2//////
+////2//////
 
 
 
-// // let count1=0;
-// // let count2=0;
-// // let remainder1=1;
-// // let remainder2=1;
+// let count1=0;
+// let count2=0;
+// let remainder1=1;
+// let remainder2=1;
 
-// // function count(a,b){
-// //     while(a!=0)
-// // {
-// //     remainder1=a%10;
-// //     if(remainder1%2==0)
-// //     {
-// //         count1++;
-// //     }
-// //     a=parseInt(a/10);
-// // }
+// function count(a,b){
+//     while(a!=0)
+// {
+//     remainder1=a%10;
+//     if(remainder1%2==0)
+//     {
+//         count1++;
+//     }
+//     a=parseInt(a/10);
+// }
 
-// // while(b!=0)
-// // {
-// //     remainder2=b%10;
-// //     if(remainder2%2==0)
-// //     {
-// //         count2++;
-// //     }
-// //     b=parseInt(b/10);
-// // }
-// // let total=count1+count2;
-// // return(total);
-// // }
-// // let a=parseInt(prompt("Enter the number"))
-// // let b=parseInt(prompt("Enter the number"))
-// // console.log(count(a,b))
-
-
-
-
-// // // // ///////3////////
-// // // // // let a=parseInt(prompt("Enter the number"))
-// // // // // let y=a**(1/2);
-// // // // // let c=parseInt(a/y)
-
-// // // // // if(y==c)
-// // // // // {
-// // // // //     console.log("yes")
-// // // // // }
-// // // // // else{
-// // // // //     console.log("no")
-// // // // // }
-
-
-// // // // function persq(a)
-// // // // {
-// // // //   let y=a**(1/2);
-// // // // let c=parseInt(a/y)
-
-// // // // if(y==c)
-// // // // {
-// // // //     console.log("yes")
-// // // // }
-// // // // else{
-// // // //     console.log("no")
-// // // // }
-// // // //   return(y);
-// // // // }
-// // // // let a=parseInt(prompt("Enter the number"))
-// // // // console.log(persq(a));
+// while(b!=0)
+// {
+//     remainder2=b%10;
+//     if(remainder2%2==0)
+//     {
+//         count2++;
+//     }
+//     b=parseInt(b/10);
+// }
+// let total=count1+count2;
+// return(total);
+// }
+// let a=parseInt(prompt("Enter the number"))
+// let b=parseInt(prompt("Enter the number"))
+// console.log(count(a,b))
 
 
 
-// // // // /////4///////
-// // // function salary(sal)
+
+// // // ///////3////////
+
+
+// // // function persq(a)
 // // // {
-// // //     sal=sal+hours*50;
-// // //     return sal;
+// // //   let y=a**(1/2);
+// // // let c=parseInt(a/y)
+
+// // // if(y==c)
+// // // {
+// // //     console.log("It is perfect square number")
 // // // }
-// // // let sal=parseInt(prompt("Enter the salary"))
-// // // let hours=parseInt(prompt("Enter the number of extra hours worked"));
-// // // console.log(salary(sal));
+// // // else{
+// // //     console.log("It is not perfect square number")
+// // // }
+// // //   return(y);
+// // // }
+// // // let a=parseInt(prompt("Enter the number"))
+// // // console.log(persq(a));
+
+
+
+// // // /////4///////
+// // function salary(sal)
+// // {
+// //     sal=sal+hours*50;
+// //     return sal;
+// // }
+// // let sal=parseInt(prompt("Enter the salary"))
+// // let hours=parseInt(prompt("Enter the number of extra hours worked"));
+// // console.log(salary(sal));
+
+
+
