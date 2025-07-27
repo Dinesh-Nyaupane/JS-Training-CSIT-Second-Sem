2f2a9a20d6e124b527b5cd068a10a9f75c7692cb
gauravgautam <gautamgaurav2063@gmail.com>
2025-07-27 18:27:03 +0545

    update  work

--- DOM/Day10/img.js
+++ DOM/Day10/img.js
@@ -10,17 +10,29 @@ Object.assign(image.style, {
   justifyContent: "center",
   gap: "10px",
 });
-img1.style.overflow="hidden";
-img1.style.width+ "300px";
-img1.style.height= "200px"
-p.style.display = "none";
+Object.assign(img1.style, {
+  overflow: "hidden",
+  width: "300px",
+  height: "200px",
+});
+Object.assign(p.style, {
+  display: "none",
+});
 img1.addEventListener("mouseover", () => {
-  image1.style.scale= "1.2";
-  image1.style.transition = "0.8s ease-in-out";
-  p.style.display = "flex";
-  p.style.backgroundColor = "yellow";
+  Object.assign(image1.style, {
+    scale:"1.2",
+    transition: "0.8s ease-in-out",
+  });
+  Object.assign(p.style, {
+    display: "flex",
+    backgroundColor: "yellow",
+  });
 });
 img1.addEventListener("mouseout", () => {
-  image1.style.scale = "1";
-  p.style.display = "none";
+  Object.assign(image1.style, {
+   scale:"1",
+  });
+  Object.assign(p.style, {
+    display: "none",
+  });
 });
