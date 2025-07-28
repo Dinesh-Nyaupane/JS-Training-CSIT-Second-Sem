03376adabd55e0aae5b72000caa11287c1155034
gauravgautam <gautamgaurav2063@gmail.com>
2025-07-28 13:11:32 +0545

    update  work

--- MiniProject-July-28-DOM/To-do List/script.js
+++ MiniProject-July-28-DOM/To-do List/script.js
@@ -5,19 +5,35 @@ const button = document.getElementById("btn");
 
 button.addEventListener("click", (event) => {
   event.preventDefault();
+
   const listItem = document.createElement("li");
   listShow.appendChild(listItem);
+
   const checkbox = document.createElement("input");
   checkbox.setAttribute("type", "checkbox");
+
   listItem.appendChild(checkbox);
+
   const taskText = document.createElement("p");
   taskText.textContent = listInput.value;
-  listItem.appendChild(taskText); 
+
+  listItem.appendChild(taskText);
+
   const deleteButton = document.createElement("button");
   deleteButton.innerText = `Delete`;
   listItem.appendChild(deleteButton);
+
   deleteButton.addEventListener("click", () => {
     listShow.removeChild(listItem);
   });
+
+  checkbox.addEventListener("change", () => {
+    if (checkbox.checked) {
+      taskText.style.textDecoration = "line-through";
+    } else {
+      taskText.style.textDecoration = "none";
+    }
+  });
+
   listInput.value = "";
 });
